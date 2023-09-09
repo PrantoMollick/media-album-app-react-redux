2513aa017adad52fa010c3e2ab59ed37a30d264c
Pranto mollick <prantomollick01@gmail.com>
2023-09-10 00:19:00 +0600

    handling errors with users creating

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {}
+    extend: {
+      keyframes: {
+        shimmer: {
+          '100%': { transform: 'translateX(100%)' }, // Defines a keyframe for the "shimmer" animation.
+        },
+      },
+      animation: {
+        shimmer: 'shimmer 1.5s infinite', // Defines an animation named "shimmer" that uses the "shimmer" keyframe.
+      },
+    },
   },
-  plugins: []
+  plugins: [],
 };
